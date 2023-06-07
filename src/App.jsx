9659f64ac8f45788dc57ab15de5fd3ad0efd08e7
ffierro4 <facundofierro49@gmail.com>
2023-06-07 17:10:35 -0300

    correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import Home from "./pages/Home";
 import Following from "./pages/Following";
 import Followers from "./pages/Followers";
 import Error from "./pages/404";
-import Aside from "./components/Aside";
 
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/aside" element={<Aside />} />
       <Route path="/" element={<Home />} />
       <Route path="/profile/:id" element={<Profile />} />
       <Route path="/followers/:id" element={<Followers />} />
