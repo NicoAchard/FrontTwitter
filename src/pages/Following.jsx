7ea11a6d28db182b0cf45254edcbe23e6eeaecc5
NicoAchard <achardn@gmail.com>
2023-06-08 21:03:01 -0300

    following.jsx

--- src/pages/Following.jsx
+++ src/pages/Following.jsx
@@ -30,56 +30,60 @@ function Following() {
   }, []);
 
   return (
-    <div class="container">
-      <div class="row">
-        <Sidebar />
-        <main class="col-10 col-md-6 border border-2 p-0 d-flex flex-column">
-          <div class="d-flex col-auto-3 align-items-center">
-            <a href="/usuarios/<%=user.username%>" class="">
-              <i class="bi bi-arrow-left text-black ms-2"> </i>
-            </a>
-            <div class="ms-4 mt-2">
+    <div className="container">
+      <div className="row">
+        <Sidebar user={user} />
+        <main className="col-10 col-md-6 border border-2 p-0 d-flex flex-column">
+          <div className="d-flex col-auto-3 align-items-center">
+            <Link to="/usuarios/<%=user.username%>">
+              <i className="bi bi-arrow-left text-black ms-2"> </i>
+            </Link>
+            <div className="ms-4 mt-2">
               <h5>
                 {user.firstname} {user.lastname}
               </h5>
-              <p class="text-secondary">{user.username}</p>
+              <p className="text-secondary">{user.username}</p>
             </div>
           </div>
-          <div class="row">
-            <div class="col d-inline-flex justify-content-center align-items-center">
-              <a
-                href="/usuarios/followers"
-                class="text-black"
-                style="text-decoration: none"
+          <div className="row">
+            <div className="col d-inline-flex justify-content-center align-items-center">
+              <Link
+                to="/usuarios/followers"
+                className="text-black"
+                style={{ textDecoration: "none" }}
               >
                 Followers
-              </a>
+              </Link>
             </div>
-            <div class="col d-inline-flex justify-content-center align-items-center">
-              <a
-                href="/usuarios/following"
-                class="text-black fw-bold text-decoration-none pb-3 border-bottom border-3"
-                style="border-color: rgba(29, 155, 240, 1) !important"
+            <div className="col d-inline-flex justify-content-center align-items-center">
+              <Link
+                to="/usuarios/following"
+                className="text-black fw-bold text-decoration-none pb-3 border-bottom border-3"
+                style={{ borderColor: "rgba(29, 155, 240, 1)" }}
               >
                 Following
-              </a>
+              </Link>
             </div>
           </div>
           <hr />
-          <div class="row d-inline-flex justify-content-between align-items-center">
-            <div class="col flex-column d-flex gap-2 align-items-center p-2">
-              <div class="d-flex justify-content-between w-100 p-3">
-                <div class="d-flex align-items-center gap-3">
-                  <a
-                    class="btn btn-primary rounded-pill"
-                    style="background-color: #cad8db; border: none"
-                    href="/tweet"
+          <div className="row d-inline-flex justify-content-between align-items-center">
+            <div className="col flex-column d-flex gap-2 align-items-center p-2">
+              <div className="d-flex justify-content-between w-100 p-3">
+                <div className="d-flex align-items-center gap-3">
+                  <Link
+                    className="btn btn-primary rounded-pill"
+                    style={{ backgroundColor: "#cad8db", border: "none" }}
+                    to="/tweet"
                   >
-                    <i class="fa-solid fa-user" style="color: #647788"></i>
-                  </a>
-                  <div class="d-flex flex-column">
-                    <span style="font-size: 1.2rem"> {following.username}</span>
-                    <span class="text-secondary"> @ {following.username}</span>
+                    <i className="fa-solid fa-user" style="color: #647788"></i>
+                  </Link>
+                  <div className="d-flex flex-column">
+                    <span style={{ fontSize: "1.2rem" }}>
+                      {following.username}
+                    </span>
+                    <span className="text-secondary">
+                      @ {following.username}
+                    </span>
                   </div>
                 </div>
                 <div>
@@ -87,8 +91,8 @@ function Following() {
                     <input type="hidden" name="id" value="<%=follower._id%>" />
                     <button
                       type="submit"
-                      class="btn btn-light rounded-pill fw-bold border-1 border-black"
-                      style="background-color: white"
+                      className="btn btn-light rounded-pill fw-bold border-1 border-black"
+                      style={{ backgroundColor: "white" }}
                     >
                       Following
                     </button>
