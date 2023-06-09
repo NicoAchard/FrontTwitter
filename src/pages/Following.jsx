adf1f49c51a7ec5e5e9a9c1a48a47e0ff61c2369
NicoAchard <achardn@gmail.com>
2023-06-08 21:26:28 -0300

    following

--- src/pages/Following.jsx
+++ src/pages/Following.jsx
@@ -33,8 +33,8 @@ function Following() {
     <div className="container">
       <div className="row">
         <Sidebar user={user} />
-        <main className="col-10 col-md-6 border border-2 p-0 d-flex flex-column">
-          <div className="d-flex col-auto-3 align-items-center">
+        <main className="col-10 col-md-6 border border-2 p-0 ">
+          <div className="d-flex align-items-center">
             <Link to="/usuarios/<%=user.username%>">
               <i className="bi bi-arrow-left text-black ms-2"> </i>
             </Link>
@@ -45,38 +45,42 @@ function Following() {
               <p className="text-secondary">{user.username}</p>
             </div>
           </div>
-          <div className="row">
-            <div className="col d-inline-flex justify-content-center align-items-center">
-              <Link
-                to="/usuarios/followers"
-                className="text-black"
-                style={{ textDecoration: "none" }}
-              >
-                Followers
-              </Link>
-            </div>
-            <div className="col d-inline-flex justify-content-center align-items-center">
-              <Link
-                to="/usuarios/following"
-                className="text-black fw-bold text-decoration-none pb-3 border-bottom border-3"
-                style={{ borderColor: "rgba(29, 155, 240, 1)" }}
-              >
-                Following
-              </Link>
-            </div>
+          <div className="row d-flex justify-content-around border-bottom border-1">
+            <Link
+              to="/usuarios/followers"
+              className="text-black text-decoration-none"
+            >
+              Followers
+            </Link>
+
+            <Link
+              to="/usuarios/following"
+              className="text-black fw-bold text-decoration-none pb-3 border-bottom border-4 border-bottom-blue"
+            >
+              Following
+            </Link>
           </div>
-          <hr />
-          <div className="row d-inline-flex justify-content-between align-items-center">
-            <div className="col flex-column d-flex gap-2 align-items-center p-2">
-              <div className="d-flex justify-content-between w-100 p-3">
-                <div className="d-flex align-items-center gap-3">
-                  <Link
+
+          <div className="d-flex flex-column gap-2 align-items-center p-2">
+            {/* <Link
                     className="btn btn-primary rounded-pill"
                     style={{ backgroundColor: "#cad8db", border: "none" }}
                     to="/tweet"
                   >
                     <i className="fa-solid fa-user" style="color: #647788"></i>
-                  </Link>
+                  </Link> */}
+            {user.following.map((following) => (
+              <div
+                className="d-flex justify-content-between w-100 p-3"
+                key={following._id}
+              >
+                <div className="d-flex align-items-center gap-3">
+                  <img
+                    src={following.profilePicture}
+                    alt="picture"
+                    className="rounded-circle img-avatar"
+                  />
+
                   <div className="d-flex flex-column">
                     <span style={{ fontSize: "1.2rem" }}>
                       {following.username}
@@ -99,7 +103,7 @@ function Following() {
                   </form>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </main>
         <Aside />
