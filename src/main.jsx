beceb5df5105eaaebb895981d2e745d424466077
ffierro4 <facundofierro49@gmail.com>
2023-06-12 16:03:39 -0300

    redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/storeConfig";
+import { store, persistor } from "./redux/storeConfig";
 import App from "./App.jsx";
 import "./index.css";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/storeConfig.js
+++ src/redux/storeConfig.js
@@ -2,11 +2,38 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice.js";
 import tweetReducer from "./tweetSlice.js";
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    tweets: tweetReducer,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  tweets: tweetReducer,
 });
 
-export default store;
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
